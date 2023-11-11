5808db03902082d6f993ab390a65e9c6be39e609
Joshua Lee <joshrhily@Joshuas-MacBook-Air.local>
2023-11-11 06:05:13 -0800

    added supabse service key

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Card from './components/card'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://icsbgxejxcvkvlkxatue.supabase.co', process.env.SUPABASE_SECRET)
+const supabase = createClient('https://icsbgxejxcvkvlkxatue.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 export const revalidate = 0
 
@@ -17,7 +17,7 @@ export default async function Home() {
     <div>
       <Navbar />
       <div>
-        <PageTitle title="Hello, World!" />
+        <PageTitle title="Meet the Team" />
         <PageContent content="Welcome to HTT's web site! HTT has been playing competitively since August 2023. Although we are a relatively new team, we all have over a year of experience playing recreationally. HTT stands a Hmong term for 'Fresh off the boat.' One of the captains, Keng, came up with the team name and the team agreed to it because it was unique and funny. Most of our team members have been friends since high school, so our team chemistry is solid." />
       </div>
       <div className="flex flex-wrap justify-center space-between min-w-full">
