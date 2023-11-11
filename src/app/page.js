c0a21f8ff41507f764602ec79645cef7753fed56
Joshua Lee <joshrhily@Joshuas-MacBook-Air.local>
2023-11-11 00:33:31 -0800

    Resized images

--- src/app/page.js
+++ src/app/page.js
@@ -15,19 +15,19 @@ export default function Home() {
         <Card
           title="Joshua Lee"
           subtitle="Outside Hitter/Setter"
-          img="http://placekitten.com/50/50"
+          img="http://placekitten.com/200/200"
           description="A reliable outside hitter for HTT that can play in any position."
         />
         <Card
           title="Chee Xiong"
           subtitle="Outside Hitter/Left Back"
-          img="http://placekitten.com/50/50"
+          img="http://placekitten.com/250/250"
           description="The little giant of HTT. Chee can jump the highest on out of the whole team and is a very strong hitter. His receiving and defense is exceptional which is why he also plays back-row"
         />
         <Card
           title="Shawn Yang"
           subtitle="Setter"
-          img="http://placekitten.com/50/50"
+          img="http://placekitten.com/300/300"
           description="Shawn is still a high school student, but has the setting skills to compete with the older setters. He has high-precision sets and can adjust them to give his hitters the right sets to score."
         />
       </div>
